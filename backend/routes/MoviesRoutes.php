ebbf64f0f4d9c0dabb1431e81c3b04198d9cfd24
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-08-23 19:49:16 -0300

    feat(api): adicionar endpoints relacionados a filmes

--- backend/routes/MoviesRoutes.php
+++ backend/routes/MoviesRoutes.php
@@ -4,8 +4,18 @@ use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('movies')->group(function () {
-    Route::get('/search', [MoviesController::class, 'search']);
-    Route::get('/genres', [MoviesController::class, 'genres']);
-    Route::get('/trending', [MoviesController::class, 'trending']);
-    Route::get('/{movieId}', [MoviesController::class, 'show']);
+    Route::middleware(['auth:api'])->group(function () {
+        Route::get('/search', [MoviesController::class, 'search']);
+        Route::get('/genres', [MoviesController::class, 'genres']);
+        Route::get('/trending', [MoviesController::class, 'trending']);
+
+        Route::get('/favorites', [MoviesController::class, 'getFavoriteMovies']);
+        Route::post('/favorites/{movieId}', [MoviesController::class, 'addFavorite']);
+        Route::delete('/favorites/{movieId}', [MoviesController::class, 'removeFromFavorite']);
+
+        Route::get('/genre/{genreId}', [MoviesController::class, 'getMoviesByGenre']);
+
+        Route::get('/{movieId}', [MoviesController::class, 'show']);
+
+    });
 });
