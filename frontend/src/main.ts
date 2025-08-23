da3e9b946b156c0cbd6f19560bf99cc003c01a42
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-08-23 20:02:22 -0300

    chore(deps): importar componentes do PrimeVue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,9 +3,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// PrimeVue
 import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice'
+
+import 'primeicons/primeicons.css'
 
 import './assets/styles/main.css'
 
@@ -14,12 +15,7 @@ const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
-app.use(PrimeVue, {
-  theme: {
-    name: 'lara-dark-blue',
-    dark: true
-  }
-})
+app.use(PrimeVue)
 app.use(ToastService)
 
 app.mount('#app')
